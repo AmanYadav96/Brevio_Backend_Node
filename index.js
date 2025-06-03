c6e6093f2ffc14f55749c9f8b547ea16d4b3036c
Aman Yadav <virajgurjar789@gmail.com>
2025-06-03 06:53:16 +0530

    fi x orogin

--- index.js
+++ index.js
@@ -58,7 +58,16 @@ app.use(async (req, res, next) => {
 
 // Middleware
 app.use(morgan('dev'));
-app.use(cors());
+
+// Updated CORS configuration
+app.use(cors({
+  origin: ['https://amans-project.netlify.app', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 204
+}));
+
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
